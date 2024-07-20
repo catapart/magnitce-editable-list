5d3658716e27025d3db64a684a784166ff708854
catapart <catapart@outlook.com>
2024-07-20 14:32:45 -0500

    added inline documentation;

--- editable-list.ts
+++ editable-list.ts
@@ -1,14 +1,17 @@
-export type EdiableListAttributes = 
+/** Attributes for configuring the EditableListElement */
+export type EditableListAttributes = 
 {
     remove?: boolean;
     edit?: boolean;
 };
-export type EditableListProperties = EdiableListAttributes &
+/** Properties for creating an EditableListElement using the `create` method */
+export type EditableListProperties = EditableListAttributes &
 {
     onAdd?: (event?: Event) => void|Promise<void>;
     onRemove?: (event?: Event) => void|Promise<void>;
     onEdit?: (event?: Event) => void|Promise<void>;
 };
+/** `part` attribute values for querying and selecting `shadowRoot` elements in the `editable-list` component */
 export enum EditableListPart
 {
     Items = "items",
@@ -17,20 +20,23 @@ export enum EditableListPart
     EditButton = "edit",
     RemoveButton = "remove",
 }
-
+/** `part` attribute values for assigning remove button and edit button templates */
 export enum ButtonTemplatePart
 {
     RemoveButton = "remove-button",
     EditButton = "edit-button",
 }
 
+/** `HTMLElement tags that are not managed by the `editable-list` element */
 const IGNORED_TAGS = new Set([
     'style',
     'template'
 ]);
 
+/** The `shadowRoot` content definition */
 const HTML = `<div part="${EditableListPart.Items}"><slot part="${EditableListPart.ItemsSlot}"></slot></div>
 <slot name="add"><button part="${EditableListPart.AddButton}" type="button">&plus;</button></slot>`;
+/** Default styles for the `editable-list` element */
 const STYLE = `
 * { box-sizing: border-box; }
 :host
@@ -51,17 +57,26 @@ const COMPONENT_STYLESHEET = new CSSStyleSheet();
 COMPONENT_STYLESHEET.replaceSync(STYLE);
 
 const COMPONENT_TAG_NAME = 'editable-list';
+/** A custom html element that adds a remove button to its children and removes the child when the button is pressed */
 export class EditableListElement extends HTMLElement
 {
+    /** if `true`, allows child elements to be removed from the DOM when their remove button is pressed */
     canRemove: boolean = true;
+    /** if `true`, allows the edit button to be added and the edit event to be dispatched */
     canEdit: boolean = false;
-    hasButtonsWithoutHandlers: boolean = false;
+    // hasButtonsWithoutHandlers: boolean = false;
 
     #boundEventHandlers: Map<string, (event?:Event) => void> = new Map([
-        ['add', this.addButton_onClick.bind(this)]
+        ['add', this.#addButton_onClick.bind(this)]
     ]);
 
+    /** Cached references to parts of this element, using the `part` attribute values as keys */
     componentParts: Map<string, HTMLElement> = new Map();
+    /**
+     * Get and cache an HTMLElement in this element's `shadowDOM` by it's `part` attribute value.
+     * @param key the `part` attribute value of the `shadowDOM`'s target child element.
+     * @returns the target child element
+     */
     getPart<T extends HTMLElement = HTMLElement>(key: string)
     {
         if(this.componentParts.get(key) == null)
@@ -72,6 +87,12 @@ export class EditableListElement extends HTMLElement
 
         return this.componentParts.get(key) as T;
     }
+    /**
+     * Get an HTMLElement in this element's `shadowDOM` by it's `part` attribute value.  
+     * Unlike `getPart`, this method does not cache the value to keep in runtime memory.
+     * @param key the `part` attribute value of the `shadowDOM`'s target child element.
+     * @returns the target child element
+     */
     findPart<T extends HTMLElement = HTMLElement>(key: string) { return this.shadowRoot!.querySelector(`[part="${key}"]`) as T; }
 
     constructor()
@@ -82,9 +103,15 @@ export class EditableListElement extends HTMLElement
         this.shadowRoot!.adoptedStyleSheets.push(COMPONENT_STYLESHEET);
 
         this.findPart(EditableListPart.AddButton)?.addEventListener('click', this.#boundEventHandlers.get('add')!);
-        this.getPart<HTMLSlotElement>(EditableListPart.ItemsSlot).addEventListener('slotchange', this.updateItemButtons.bind(this))
+        this.getPart<HTMLSlotElement>(EditableListPart.ItemsSlot).addEventListener('slotchange', this.#updateItemButtons.bind(this))
         
     }
+
+    /**
+     * Create a new instance of an `EditableListElement` element using the provided properties to define the configuration.
+     * @param props target `EditableListProperties` values
+     * @returns a configured instance of an `EditableListElement` element.
+     */
     static create(props?: EditableListProperties)
     {
         const element = document.createElement(COMPONENT_TAG_NAME) as EditableListElement;
@@ -109,12 +136,18 @@ export class EditableListElement extends HTMLElement
         return element;
     }
 
-    addButton_onClick()
+    /**
+     * Handle click on the "Add Item" button
+     */
+    #addButton_onClick()
     {
         this.dispatchEvent(new CustomEvent('add'));
     }
 
-    updateItemButtons()
+    /**
+     * Iterate through slot children to add buttons and listeners where applicable.
+     */
+    #updateItemButtons()
     {
         const children = this.getPart<HTMLSlotElement>(EditableListPart.ItemsSlot).assignedElements();
         for(let i = 0; i < children.length; i++)
@@ -162,19 +195,19 @@ export class EditableListElement extends HTMLElement
                     });
                     item.appendChild(editButton);
                 }
-                else if(this.hasButtonsWithoutHandlers)
-                {
-                    existingEditButton.addEventListener('click', (event: Event) =>
-                    { 
-                        this.dispatchEvent(new CustomEvent('edit', { detail: item }));
-                        if(this.hasAttribute('cancel-edit'))
-                        {
-                            event.preventDefault();
-                            event.stopPropagation();
-                            return false;
-                        }
-                    });
-                }
+                // else if(this.hasButtonsWithoutHandlers)
+                // {
+                //     existingEditButton.addEventListener('click', (event: Event) =>
+                //     { 
+                //         this.dispatchEvent(new CustomEvent('edit', { detail: item }));
+                //         if(this.hasAttribute('cancel-edit'))
+                //         {
+                //             event.preventDefault();
+                //             event.stopPropagation();
+                //             return false;
+                //         }
+                //     });
+                // }
             }
             else if(existingEditButton != null)
             {
@@ -211,20 +244,20 @@ export class EditableListElement extends HTMLElement
                     });
                     item.appendChild(removeButton);
                 }
-                else if(this.hasButtonsWithoutHandlers)
-                {
-                    existingRemoveButton.addEventListener('click', (event: Event) =>
-                    { 
-                        item.remove();
-                        this.dispatchEvent(new CustomEvent('remove', { detail: item }));
-                        if(this.hasAttribute('cancel-remove'))
-                        {
-                            event.preventDefault();
-                            event.stopPropagation();
-                            return false;
-                        }
-                    });
-                }
+                // else if(this.hasButtonsWithoutHandlers)
+                // {
+                //     existingRemoveButton.addEventListener('click', (event: Event) =>
+                //     { 
+                //         item.remove();
+                //         this.dispatchEvent(new CustomEvent('remove', { detail: item }));
+                //         if(this.hasAttribute('cancel-remove'))
+                //         {
+                //             event.preventDefault();
+                //             event.stopPropagation();
+                //             return false;
+                //         }
+                //     });
+                // }
             }
             else if(existingRemoveButton != null)
             {
@@ -238,6 +271,12 @@ export class EditableListElement extends HTMLElement
         'remove',
         'edit',
     ];
+    /**
+     * Update items to new configuration when attributes change
+     * @param attributeName the attribute that has changed
+     * @param _oldValue the previous value that the updating attribute was assinged as
+     * @param newValue the new value that the updating attribute is assigned as
+     */
     attributeChangedCallback(attributeName: string, _oldValue: string, newValue: string) 
     {
         if(attributeName == "remove")
@@ -250,7 +289,7 @@ export class EditableListElement extends HTMLElement
             {
                 this.canRemove = false;
             }
-            this.updateItemButtons();
+            this.#updateItemButtons();
         }
         else if(attributeName == "edit")
         {
@@ -262,7 +301,7 @@ export class EditableListElement extends HTMLElement
             {
                 this.canEdit = true;
             }
-            this.updateItemButtons();
+            this.#updateItemButtons();
         }
     }
 }
